74413191ed6a5a9a296a53e2fa69a7ebdce03c19
leiax <leiax00@outlook.com>
2019-03-13 22:38:40 +0800

    redis中对于lua脚本的调试

---
+++ src/lua/redis_lua_debugger.lua
@@ -0,0 +1,30 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: leiax
+-- Date: 2019/3/13
+-- Time: 21:49
+-- To change this template use File | Settings | File Templates.
+--
+---------------------  redis debugger setting  ------------------
+local redis = require 'redis'
+local host = "127.0.0.1"
+local port = 6379
+client = redis.connect(host, port)
+redis.call = function(cmd, ...)
+    return assert(loadstring('return client:'.. string.lower(cmd) ..'(...)'))(...)
+end
+-----------------------  methods theme  ------------------------
+local function redisOperator(a, b)
+    redis.call('set', 'a', 'a')
+    redis.call('set', 'b', 'b')
+    redis.call('set', 'lua:a', a)
+    redis.call('set', 'lua:b', b)
+    local keys = redis.call('keys', '*')
+    return keys
+end
+
+----------------------  execute theme  --------------------------
+local a = 2
+local b = 33
+local result = redisOperator(a, b)
+print(table.concat(result,', '))
