2d5771b4713fc0bb357295f3387f086625267f76
Leiax00 <leiax00@outlook.com>
2019-09-11 21:57:04 +0800

    添加pprof监控

--- src/beeWeb/main.go
+++ src/beeWeb/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"github.com/astaxie/beego"
 	_ "github.com/beeWeb/routers"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		http.ListenAndServe("0.0.0.0:8090", nil)
+	}()
 	beego.Run()
 }
