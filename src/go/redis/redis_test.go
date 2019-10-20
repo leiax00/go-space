eaf88606dd5afb36bfe1c40b233aaa9c57c7818e
Leiax00 <leiax00@outlook.com>
2019-10-20 13:20:17 +0800

    测试redis结果封装

--- src/go/redis/redis_test.go
+++ src/go/redis/redis_test.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"encoding"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
@@ -13,11 +15,29 @@ func TestSetFunc(t *testing.T) {
 		Password: "root", // no password set
 		DB:       0,      // use default DB
 	})
-	result, err := client.Set("as", "as1", 10*time.Second).Result()
+	result, err := client.Set("as", "as1", 2*time.Second).Result()
 	fmt.Println(result, err)
 	result, err = client.Get("as").Result()
 	fmt.Println(result, err)
-	time.Sleep(10 * time.Second)
+	time.Sleep(2 * time.Second)
 	result, err = client.Get("as").Result()
 	fmt.Println(result, err)
+
+	result, err = client.HMSet("a3", map[string]interface{}{"a1": "{\"name\":\"aaa\", \"age\":18}", "a2": "a22"}).Result()
+	fmt.Println(result, err)
+	var result1 = &User{}
+	err = client.HGet("a3", "a1").Scan(result1)
+	fmt.Println(result1, err)
+
+}
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var _ encoding.BinaryUnmarshaler = (*User)(nil)
+
+func (user *User) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, &user)
 }
