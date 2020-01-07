7db3238233cabafebd813cfcdbe5718bcc2c8bc6
Leiax00 <leiax00@outlook.com>
2020-01-07 23:55:52 +0800

    优化：lua实现大数字的四则运算-加减

--- src/lua/big_num_calc.lua
+++ src/lua/big_num_calc.lua
@@ -1,24 +1,19 @@
 local function build(str)
-    local n, c = math.floor(#str / 4), {}
+    local n, c, s = math.floor(#str / 4), {}, 1
     if #str % 4 ~= 0 then
-        c[n + 1] = tonumber(string.sub(str, 4 * n + 1, #str))
+        c[n + 1], s = tonumber(string.sub(str, 1, #str % 4)), #str % 4 + 1
     end
-    for i = 1, n, 1 do
-        c[i] = tonumber(string.sub(str, 4 * i - 3, 4 * i))
+    for i = n, 1, -1 do
+        c[i], s = tonumber(string.sub(str, s, s + 3)), s + 4
     end
     return c
 end
 
 local function get(c)
     local r = ""
-    for i, t in pairs(c) do
-        if tonumber(c[1]) < 0 and tonumber(t) > 0 then
-            local v = math.pow(10, #t) - tonumber(t)
-            r = string.sub(r, 1, #r - 1) .. tostring(tonumber(string.sub(r, #r)) - 1) .. tostring(v)
-        else
-            if r ~= '' or tonumber(t) ~= 0 then
-                r = r .. t
-            end
+    for i = #c, 1, -1 do
+        if r ~= '' or tonumber(c[i]) ~= 0 then
+            r = r .. c[i]
         end
     end
     return r ~= '' and r or '0'
@@ -27,16 +22,10 @@ end
 local function add(a, b)
     local c, d = {}, 0
     a, b = build(a), build(b)
-    local delta, aMax = math.abs(#a - #b), #a > #b
-    for i = math.max(#a, #b), 1, -1 do
-        local aa = aMax and (a[i] or 0) or (a[i - delta] or 0)
-        local bb = aMax and (b[i - delta] or 0) or (b[i] or 0)
-        local t = d + aa + bb
-        if i ~= 1 and #tostring(t) > #tostring(math.max(aa, bb)) then
-            c[i], d = string.sub(tostring(t), 2), 1
-        else
-            c[i], d = tostring(t), 0
-        end
+    local count = math.max(#a, #b)
+    for i = 1, count, 1 do
+        local t = d + (a[i] or 0) + (b[i] or 0)
+        c[i], d = i ~= count and tostring(t % 10000), math.floor(t/10000) or tostring(t), 0
     end
     return get(c)
 end
@@ -67,12 +56,12 @@ local function div(a, b)
 
 end
 
-local c = add("1234567891234567890", "1234000000000000220")
-print(c)
+--local c = add("1234567891234567890", "1234000000000000220")
+--print(c)
 print(add('1200', '130000000'))
-print(add('1200', '1300'))
-print(sub('1200', '1300'))
-print(sub('1300', '1200'))
-print(sub('123456789', '123456789'))
-print(sub('123456789', '1234567890123'))
-print(sub('1234567890123', '123456789'))
+--print(add('1200', '1300'))
+--print(sub('1200', '1300'))
+--print(sub('1300', '1200'))
+--print(sub('123456789', '123456789'))
+--print(sub('123456789', '1234567890123'))
+--print(sub('1234567890123', '123456789'))
